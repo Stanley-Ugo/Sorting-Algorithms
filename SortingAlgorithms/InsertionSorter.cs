55904b1d23878d853f7e5c84832305fea7913e39
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-01-03 15:56:04 +0100

    looping through & Swapping values at indices atfer comparisons

--- SortingAlgorithms/InsertionSorter.cs
+++ SortingAlgorithms/InsertionSorter.cs
@@ -6,7 +6,20 @@ namespace SortingAlgorithms
     {
         public static void DoInsertionSort(this int[] numbers)
         {
+            for (int i = 1; i < numbers.Length; ++i)
+            {
+                for (int j = i; j > 0 && numbers[j -1] > numbers[j]; j--)
+                {
+                    numbers.SwappValuesAtIndices(j, j - 1);
+                }
+            }
+        }
 
+        public static void SwappValuesAtIndices(this int[] numbers, int i, int j)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
         }
     }
 }
