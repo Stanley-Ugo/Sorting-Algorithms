a7384e95ddb6038649e984f1c26692823a270e74
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-01-03 16:17:54 +0100

    Final Commit - Calling TestInsertionSort() in the Main method

--- SortingAlgorithmsConsoleApp/Program.cs
+++ SortingAlgorithmsConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SortingAlgorithms;
+using System;
 
 namespace SortingAlgorithmsConsoleApp
 {
@@ -6,7 +7,51 @@ namespace SortingAlgorithmsConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            TestInsertionSort();
+            
+        }
+
+        private static void TestInsertionSort()
+        {
+            Console.WriteLine("Pleadse Insert 10 Nunmbers");
+
+            int numbers1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers6 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers7 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers8 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers9 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pleadse Insert Another Nunmbers");
+
+            int numbers10 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Press Enter To Sort");
+
+            int[] numbersArray = { numbers1, numbers2, numbers3, numbers4, numbers5, numbers6, numbers7, numbers8, numbers9, numbers10 };
+
+            numbersArray.DoInsertionSort();
+
+            foreach (int number in numbersArray)
+                Console.Write(number + " ");
         }
     }
 }
